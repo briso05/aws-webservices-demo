f5ca19ef181cb56d483e2d05542140c0c89fc901
briso05 <sobright05@naver.com>
2022-05-27 16:03:49 +0900

    create project

---
+++ src/main/java/com/jojoldu/book/briso/AwsSpringBootWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.briso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringBootWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSpringBootWebServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jojoldu/book/briso/AwsSpringBootWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.briso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringBootWebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
